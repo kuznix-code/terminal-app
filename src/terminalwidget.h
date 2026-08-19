81a8068785229c8f459089af4c5167ba833126f6
Kuznix <krzysztofdemirkuzniak@gmail.com>
2026-08-19 19:28:31 +0200

    Add terminal theming, font and robust ANSI handling

--- src/terminalwidget.h
+++ src/terminalwidget.h
@@ -1,13 +1,21 @@
 #pragma once
+
 #include <QPlainTextEdit>
 #include <QProcess>
+#include <QColor>
+#include <QFont>
+#include <functional>
 
 class TerminalWidget : public QPlainTextEdit {
 public:
     explicit TerminalWidget(QWidget *parent = nullptr);
+
     void start(const QString &shell, const QString &workingDir);
     void sendCommand(const QString &command);
     void stop();
+    void setTerminalColors(const QColor &background, const QColor &foreground, const QColor &selection);
+    void setTerminalFont(const QFont &font);
+    void setScrollback(int lines);
     QProcess *process() const { return m_process; }
 
     using ExitStatusCallback = std::function<void(int)>;
@@ -22,4 +30,5 @@ private:
     int m_promptPosition = 0;
     ExitStatusCallback m_exitStatusCallback;
     void appendOutput(const QByteArray &data);
+    QString stripAnsi(const QString &text) const;
 };
